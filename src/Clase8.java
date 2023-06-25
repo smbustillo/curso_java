62adb6ab274eed29c98b958b4bc946556121202d
Susana M. Bustillo <smbustill@gmail.com>
2023-06-25 21:00:52 +0200

    calcular area triangulo

--- src/Clase8.java
+++ src/Clase8.java
@@ -1,12 +1,23 @@
 public class Clase8 {
+    //Ejecutar funciones hechas
     public static void main(String[] args) {
         float lado = 34.2F;
+        float altura = 45.3F;
+
         float areaCuadrado = calcularAreaCuadrado(lado);
+        float areaTriangulo = calcuarAreaTriangulo(lado,altura);
 
         System.out.println("El area del cuadrado es " + areaCuadrado);
+        System.out.println("El area del triangulo es " + areaTriangulo);
     }
+
     //Hacer una función que calcule el área de un cuadrado. Al llamarla debe devolver el área del cuadrado.
     private static float calcularAreaCuadrado(float lado){
         return lado*lado;
+    }
+
+    //Hacer una función que calcule el área de un triángulo. Al llamarla debe devolver el área del triángulo.
+    private static float calcuarAreaTriangulo(float base, float altura){
+        return (base*altura)/2;
     }
 }
