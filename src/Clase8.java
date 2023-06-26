465e6eb837ffe02f67ad1f304e13c3653246be19
Susana M. Bustillo <smbustill@gmail.com>
2023-06-26 20:39:43 +0200

    cambiar nomenclatura

--- src/Clase8.java
+++ src/Clase8.java
@@ -6,8 +6,8 @@ public class Clase8 {
         float lado = 34.2F;
         float altura = 45.3F;
 
-        float areaCuadrado = calcularAreaCuadrado(lado);
-        float areaTriangulo = calcuarAreaTriangulo(lado,altura);
+        float areaCuadrado = getAreaCuadrado(lado);
+        float areaTriangulo =getAreaTriangulo(lado,altura);
 
         System.out.println("El area del cuadrado es " + areaCuadrado);
         System.out.println("El area del triangulo es " + areaTriangulo);
@@ -15,12 +15,12 @@ public class Clase8 {
     }
 
     //Hacer una función que calcule el área de un cuadrado. Al llamarla debe devolver el área del cuadrado.
-    private static float calcularAreaCuadrado(float lado){
+    private static float getAreaCuadrado(float lado){
         return lado*lado;
     }
 
     //Hacer una función que calcule el área de un triángulo. Al llamarla debe devolver el área del triángulo.
-    private static float calcuarAreaTriangulo(float base, float altura){
+    private static float getAreaTriangulo(float base, float altura){
         return (base*altura)/2;
     }
 
