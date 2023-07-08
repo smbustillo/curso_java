5aec97b23257d63443dd8141b47ae4266909a44c
Susana M. Bustillo <smbustill@gmail.com>
2023-07-08 20:36:20 +0200

    ejercicio busqueda

--- src/Clase9.java
+++ src/Clase9.java
@@ -1,18 +1,23 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Clase9 {
     public static void main(String[] args) {
+        String personaEncontrada="";
         String [] personas = {"Susana Bustillo","Lola Mosquera","Lolita Rubia","khaleesi"};
 
         System.out.print("Introduzca búsqueda: ");
 
         Scanner teclado = new Scanner(System.in);
-        String busqueda = teclado.next();
+        String busqueda = teclado.nextLine();
 
         for(int indice = 0; indice < personas.length; indice++){
-            String personaEncontrada = personas[indice];
-            System.out.println(personaEncontrada);
+            if(personas[indice].toLowerCase(Locale.ROOT).contains(busqueda.toLowerCase(Locale.ROOT))){
+                personaEncontrada = personas[indice];
+            }
         }
 
+        System.out.print("La persona es: ");
+        System.out.println(personaEncontrada);
     }
 }
